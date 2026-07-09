3cb3fbacfa4fd37c22fa77cbff3e156a8082d65f
spruceensari-cyber <spruce.ensari@gmail.com>
2026-07-10 02:54:04 +0300

    feat: connect ADS mobile action

--- Assets/RenkaiMobile/Scripts/Input/MobileActionButton.cs
+++ Assets/RenkaiMobile/Scripts/Input/MobileActionButton.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Renkai.Input;
+using RenkaiMobile.Combat;
 
 namespace RenkaiMobile.Input
 {
@@ -9,33 +10,34 @@ namespace RenkaiMobile.Input
         public enum ActionType { Fire, Ads, Crouch, Jump, Reload }
 
         [SerializeField] private MobileInputRouter router;
+        [SerializeField] private AdsController adsController;
         [SerializeField] private ActionType action;
 
-        public void Configure(MobileInputRouter inputRouter, ActionType actionType)
+        public void Configure(MobileInputRouter inputRouter, AdsController ads, ActionType actionType)
         {
             router = inputRouter;
+            adsController = ads;
             action = actionType;
         }
 
         public void OnPointerDown(PointerEventData eventData)
         {
-            if (router == null) return;
-            if (action == ActionType.Fire) router.SetFireHeld(true);
-            if (action == ActionType.Crouch) router.SetCrouch(true);
+            if (action == ActionType.Fire) router?.SetFireHeld(true);
+            if (action == ActionType.Crouch) router?.SetCrouch(true);
+            if (action == ActionType.Ads) adsController?.SetAds(true);
         }
 
         public void OnPointerUp(PointerEventData eventData)
         {
-            if (router == null) return;
-            if (action == ActionType.Fire) router.SetFireHeld(false);
-            if (action == ActionType.Crouch) router.SetCrouch(false);
+            if (action == ActionType.Fire) router?.SetFireHeld(false);
+            if (action == ActionType.Crouch) router?.SetCrouch(false);
+            if (action == ActionType.Ads) adsController?.SetAds(false);
         }
 
         public void OnPointerClick(PointerEventData eventData)
         {
-            if (router == null) return;
-            if (action == ActionType.Jump) router.Jump();
-            if (action == ActionType.Reload) router.Reload();
+            if (action == ActionType.Jump) router?.Jump();
+            if (action == ActionType.Reload) router?.Reload();
         }
     }
 }
