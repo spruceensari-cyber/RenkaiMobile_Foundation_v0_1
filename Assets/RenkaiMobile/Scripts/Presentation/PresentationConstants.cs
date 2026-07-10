dc2f37f5896ac7e77f5cf4eafb729e018969c65a
spruceensari-cyber <spruce.ensari@gmail.com>
2026-07-10 13:47:55 +0300

    chore: add presentation constants

---
+++ Assets/RenkaiMobile/Scripts/Presentation/PresentationConstants.cs
@@ -0,0 +1,8 @@
+namespace RenkaiMobile.Presentation
+{
+    public static class PresentationConstants
+    {
+        public const string DistrictName = "Kagami District";
+        public const string MatchLabel = "Resonance Match";
+    }
+}
